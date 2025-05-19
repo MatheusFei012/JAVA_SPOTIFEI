a10307e873cc1d5124b11fee62dece90550f4cf1
MatheusFei012 <mcruz20070151@gmail.com>
2025-05-18 23:30:40 -0300

    Create Ouvir.java

---
+++ model/Ouvir.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ *
+ * @author mcruz
+ */
+public class Ouvir {
+    private String nome;
+
+    public Ouvir() {}
+
+    public Ouvir(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
