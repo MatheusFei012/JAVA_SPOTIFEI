4a76db2c83e7d2cb2cb4e776c5bac73ddb839c64
MatheusFei012 <mcruz20070151@gmail.com>
2025-05-18 23:30:03 -0300

    Create Historico.java

---
+++ model/Historico.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Historico {
+    private String nome;
+
+    public Historico() {}
+
+    public Historico(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
