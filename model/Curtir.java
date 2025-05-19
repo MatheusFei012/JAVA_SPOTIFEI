3aedc9de82cba04d4119cbf827480b4b4934ce3f
MatheusFei012 <mcruz20070151@gmail.com>
2025-05-18 23:29:28 -0300

    Create Curtir.java

---
+++ model/Curtir.java
@@ -0,0 +1,21 @@
+package model;
+
+
+
+public class Curtir {
+    private String nome;
+
+    public Curtir() {}
+
+    public Curtir(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
